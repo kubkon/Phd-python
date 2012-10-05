0f7afd490bc62042decd1a2a6362074382371b2e
Jakub Konka <kubkon@gmail.com>
2012-10-05 13:37:40 +0100

    Add possibility to run only one process.

--- Auctions/Digital Marketplace/simulation/run.py
+++ Auctions/Digital Marketplace/simulation/run.py
@@ -16,24 +16,38 @@ def main():
                       type=int, help='number of repetitions')
   parser.add_argument('sim_duration', metavar='simulation_duration',
                       type=int, help='duration of each simulation stage in seconds')
+  parser.add_argument('--batch_size', dest='batch_size', default=4,
+                      type=int, help='batch size for multiprocessing')
   args = parser.parse_args()
   repetitions = args.reps
   sim_duration = args.sim_duration
+  batch_size = args.batch_size
   
   ### Run simulations
-  # Split num of repetitions into batches of 4
-  batch_size = 4
-  quotient = repetitions // batch_size
-  remainder = repetitions % batch_size
-  # Run the simulations (4 in parallel as subprocesses)
-  for n in range(1, quotient+2):
-    num_proc = batch_size if n * batch_size <= repetitions else remainder
-    for m in range(num_proc):
+  # One process at a time
+  if batch_size == 1:
+    for n in range(repetitions):
       try:
-        seed = m + batch_size * (n-1)
-        sub.Popen("python main.py {} --seed={} --id={}".format(sim_duration, seed, seed), shell=True)
+        sub.call("python main.py {} --seed={} --id={}".format(sim_duration, n, n), shell=True)
       except OSError as e:
         print("Execution failed: ", e)
+  # In batches
+  else:
+    # Split num of repetitions into batches
+    quotient = repetitions // batch_size
+    remainder = repetitions % batch_size
+    # Run the simulations in parallel as subprocesses
+    for n in range(1, quotient+2):
+      num_proc = batch_size if n * batch_size <= repetitions else remainder
+      for m in range(num_proc):
+        try:
+          seed = m + batch_size * (n-1)
+          if m < num_proc - 1:
+            sub.Popen("python main.py {} --seed={} --id={}".format(sim_duration, seed, seed), shell=True)
+          else:
+            sub.Popen("python main.py {} --seed={} --id={}".format(sim_duration, seed, seed), shell=True).wait()
+        except OSError as e:
+          print("Execution failed: ", e)
 
 
 if __name__ == '__main__':
