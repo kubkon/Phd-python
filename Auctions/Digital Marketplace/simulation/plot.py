295ef76ebe8fd9e136eceb7e1abb329936dfe444
Jakub Konka <kubkon@gmail.com>
2012-10-11 10:21:43 +0100

    Update plot.py to reflect changes to run.py.
    At the moment, plot reputation history only.

--- Auctions/Digital Marketplace/simulation/plot.py
+++ Auctions/Digital Marketplace/simulation/plot.py
@@ -9,47 +9,49 @@ Copyright (c) 2012 University of Strathclyde. All rights reserved.
 import argparse
 from csv import DictReader
 import matplotlib.pyplot as plt
+import os
 
 
 ### Parse command line arguments
-parser = argparse.ArgumentParser(description="DM simulation plotting script")
+parser = argparse.ArgumentParser(description='DM simulation plotting script')
 parser.add_argument('input_dir', metavar='input_dir',
                     help='input directory')
 args = parser.parse_args()
 input_dir = args.input_dir
 
-### Load data from files
-mean_dct = {}
-with open(input_dir + "/mean_reduced.out", 'rt') as f:
-  reader = DictReader(f)
-  for row in reader:
-    for key in row:
-      mean_dct.setdefault(key,[]).append(float(row[key]))
+### Get all file names
+extension = '.out'
+file_names = [f[:f.find(extension)] for _, _, files in os.walk(input_dir) for f in files if f.endswith(extension)]
 
-### Plot
-# Figure 1: Reputation history
-bidders = [h[len("reputation")+1:] for h in mean_dct if "reputation" in h]
+### Reputation history
+# Load data from files
+ref_column = 'sr_number'
+rep_dct = {}
+for fn in filter(lambda x: 'reputation' in x, file_names):
+  rep_dct[fn] = {}
+  with open(input_dir + '/' + fn + extension, 'rt') as f:
+    reader = DictReader(f)
+    for row in reader:
+      for key in row:
+        val = float(row[key]) if key != ref_column else int(row[key])
+        rep_dct[fn].setdefault(key,[]).append(val)
+# Plot
+# Figure 1..k=num of bidders: reps with confidence intervals
+for key in rep_dct:
+  plt.figure()
+  plt.errorbar(rep_dct[key][ref_column], rep_dct[key]['mean'], yerr=rep_dct[key]['ci'], fmt='ro')
+  plt.xlabel('Service request')
+  plt.ylabel('Reputation')
+  plt.grid()
+  plt.savefig(input_dir + '/' + key + '.pdf')
+# Figure k + 1: all reps same plot
 plt.figure()
-for b in bidders:
-  plt.plot(mean_dct["sr_number"], mean_dct["reputation" + "_" + b])
-plt.xlabel("Service request")
-plt.ylabel("Reputation")
-plt.legend(bidders)
+legend = []
+for key in rep_dct:
+  plt.plot(rep_dct[key][ref_column], rep_dct[key]['mean'])
+  legend += [key[len('reputation_'):]]
+plt.xlabel('Service request')
+plt.ylabel('Reputation')
+plt.legend(legend)
 plt.grid()
-plt.savefig(input_dir + "/reputation.pdf")
-
-# Figure 2: Prices
-plt.figure()
-plt.plot(mean_dct["sr_number"], mean_dct["price"], '.')
-plt.xlabel("Service request")
-plt.ylabel("Price")
-plt.grid()
-plt.savefig(input_dir + "/price.pdf")
-plt.ylim([0,1])
-plt.savefig(input_dir + "/price_ylim_0_1.pdf")
-plt.ylim([0,5])
-plt.savefig(input_dir + "/price_ylim_0_5.pdf")
-plt.ylim([0,10])
-plt.savefig(input_dir + "/price_ylim_0_10.pdf")
-plt.ylim([0,100])
-plt.savefig(input_dir + "/price_ylim_0_100.pdf")
+plt.savefig(input_dir + '/reputation.pdf')
