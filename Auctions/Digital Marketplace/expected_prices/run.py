90602c2f7a7797a260c349111b1d27fe11aee7fe
Jakub Konka <kubkon@gmail.com>
2012-10-16 17:08:34 +0100

    Update run.py for expected prices.
    - 10,000 replications.
    - Specific pairings of the reputation ratings.

--- Auctions/Digital Marketplace/expected_prices/run.py
+++ Auctions/Digital Marketplace/expected_prices/run.py
@@ -23,17 +23,14 @@ batch_size = args.batch_size
 save_dir = args.save_dir
 
 ### Init
-reps = [[.25,1.0], [.5,1.0], [.75,1.0], [1.0,1.0],
-        [.25,.75], [.5,.75], [.75,.75], [1.0,.75],
-        [.25,.50], [.5,.50], [.75,.50], [1.0,.50],
-        [.25,.25], [.5,.25], [.75,.25], [1.0,.25]]
+reps = [[.25,1.0], [.25,.75], [.25,.5], [.25,.25]]
 
 ### Run simulations
 try:
   # One process at a time
   if batch_size == 1:
     for r in reps:
-      sub.call("python expected_prices.py {} {} --save_dir={}".format(1000, "{},{}".format(*r), save_dir), shell=True)
+      sub.call("python expected_prices.py {} {} --save_dir={}".format(10000, "{},{}".format(*r), save_dir), shell=True)
   # In batches
   else:
     # Split num of repetitions into batches
@@ -42,7 +39,7 @@ try:
     remainder = repetitions % batch_size
     # Run the simulations in parallel as subprocesses
     num_proc = batch_size if batch_size <= repetitions else remainder
-    procs = [sub.Popen("python expected_prices.py {} {} --save_dir={}".format(1000, "{},{}".format(*reps[n]), save_dir), shell=True) for n in range(num_proc)]
+    procs = [sub.Popen("python expected_prices.py {} {} --save_dir={}".format(10000, "{},{}".format(*reps[n]), save_dir), shell=True) for n in range(num_proc)]
     while True:
       procs_poll = list(map(lambda x: x.poll() != None, procs))
       if not all(procs_poll):
@@ -50,7 +47,7 @@ try:
       elif num_proc < repetitions:
         temp_num = batch_size if num_proc + batch_size <= repetitions else remainder
         for n in range(num_proc, num_proc + temp_num):
-          procs += [sub.Popen("python expected_prices.py {} {} --save_dir={}".format(1000, "{},{}".format(*reps[n]), save_dir), shell=True)]
+          procs += [sub.Popen("python expected_prices.py {} {} --save_dir={}".format(10000, "{},{}".format(*reps[n]), save_dir), shell=True)]
         num_proc += temp_num
       else:
         break
