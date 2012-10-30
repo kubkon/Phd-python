b413f09abf2b453b840a5fb318a648caa252b313
Jakub Konka <kubkon@gmail.com>
2012-10-30 16:52:01 +0000

    Calculate max abs error for expected prices.
    Add short script for calculating maximum absolute error between expected
    prices for reversed pairs of reputation ratings:
    abs(p(0.25,0.75)-p(0.75,0.25)).

---
+++ Auctions/Digital Marketplace/expected_prices/error.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+error.py
+
+Created by Jakub Konka on 2012-10-30.
+Copyright (c) 2012 University of Strathclyde. All rights reserved.
+"""
+import argparse
+from csv import DictReader
+from itertools import combinations
+import os
+
+### Parse command line arguments
+parser = argparse.ArgumentParser(description="Expected prices in one-shot DM auction")
+parser.add_argument('save_dir', help='output directory')
+args = parser.parse_args()
+save_dir = args.save_dir
+
+### Load data from files
+print("Loading data from files...")
+extension = '.out'
+file_names = [f[:f.find(extension)] for root, _, files in os.walk(save_dir) for f in files if f.endswith(extension)]
+fn_split_set = set([item for sublist in [fn.split('_') for fn in file_names] for item in sublist])
+fn_comb = list(combinations(fn_split_set, 2))
+for comb in fn_comb:
+  try:
+    data = []
+    for i in range(2):
+      path = save_dir + '/' + '_'.join([comb[i%2], comb[(i+1)%2]]) + extension
+      with open(path, 'rt') as f:
+        reader = DictReader(f)
+        dct = {}
+        for row in reader:
+          for key in row:
+            dct.setdefault(key,[]).append(float(row[key]))
+        data.append(dct)
+
+    ### Calculate absolute errors
+    error = [abs(x-y) for (x,y) in zip(data[0]['mean'], data[1]['mean'])]
+    print("Maximum absolute error for reps {}: {}".format(comb, max(error)))
+  except IOError:
+    continue
