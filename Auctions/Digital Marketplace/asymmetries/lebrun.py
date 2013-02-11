15f1299914947e3eca259337ba41b2b8dadd81b5
Jakub Konka <kubkon@gmail.com>
2013-02-11 15:13:49 +0000

    Update algorithm for n>3 bidders.

--- Auctions/Digital Marketplace/asymmetries/lebrun.py
+++ Auctions/Digital Marketplace/asymmetries/lebrun.py
@@ -13,7 +13,7 @@ def F(bounds, x):
 
 # Scenario
 w = 0.5
-reps = [0.25, 0.5, 0.75]
+reps = [0.25, 0.75]
 n = len(reps)
 lower_extremities = [(1-w)*r for r in reps]
 upper_extremities = [(1-w)*r + w for r in reps]
@@ -62,29 +62,32 @@ while not (high - low < error):
           k = i+1
   guess_costs = [min(l, cost) if cost is not None else l for l in lower_extremities]
   print("k={}, costs: {}".format(k, guess_costs))
+  bids = np.linspace(guess_bid, b_upper-0.1, 10000)
   if k >= n:
     node = lambda ys, t: ode(n, ys, t)
-    bids = np.linspace(guess_bid, b_upper, 10000)
     tables = scint.odeint(node, guess_costs, bids)
     costs = [list(map(lambda x: x[i], tables)) for i in range(k)]
   else:
-    # 1. for all b in [b_lower, lower_extremities[2]]
-    kode = lambda ys, t: ode(k, ys, t)
-    bids = np.linspace(guess_bid, b_upper, 10000)
-    tables = scint.odeint(kode, guess_costs[:k], bids)
-    tmps = [extension(k, t, b) for t, b in zip(tables, bids)]
-    # here, need to find the value of b for which extension() gives lower extremity
-    diffs = list(map(lambda x: abs(x - lower_extremities[k]), tmps))
-    stop_index = diffs.index(min(diffs))
-    costs = [list(map(lambda x: x[i], tables[:stop_index+1])) for i in range(k)]
-    costs += [tmps[:stop_index+1]]
-    # 2. for all b in (lower_extremity[2], b_upper]
-    kode = lambda ys, t: ode(k+1, ys, t)
-    bids_2 = np.linspace(bids[stop_index], b_upper, 10000-(stop_index+1))
-    tables = scint.odeint(kode, [c[-1] for c in costs], bids_2)
-    tmps = [list(map(lambda x: x[i], tables)) for i in range(k+1)]
-    for i in range(k+1):
-      costs[i] += tmps[i]
+    def recur(k, costs, bids):
+      if k > n:
+        return costs, bids
+      else:
+        kode = lambda ys, t: ode(k, ys, t)
+        ode_tables = scint.odeint(kode, [costs[i][-1] for i in range(k)], bids)
+        if k < n:
+          ext_tables = [extension(k, t, b) for t, b in zip(ode_tables, bids)]
+          diffs = list(map(lambda x: abs(x - lower_extremities[k]), ext_tables))
+          stop_index = diffs.index(min(diffs))
+          tmps = [list(map(lambda x: x[i], ode_tables[:stop_index+1])) for i in range(k)]
+          tmps += [ext_tables[:stop_index+1] for i in range(n-k)]
+          bids = np.linspace(bids[stop_index], b_upper, len(bids)-stop_index)
+          k += lower_extremities.count(lower_extremities[k]) - 1
+        else:
+          tmps = [list(map(lambda x: x[i], ode_tables)) for i in range(k)]
+        for i in range(n):
+          costs[i] += tmps[i][1:]
+        return recur(k+1, costs, bids)
+    costs, _ = recur(k, [[c] for c in guess_costs], bids)
   condition1 = [all(map(lambda x: x >= g_cost and x <= b_upper, cost)) for g_cost, cost in zip(guess_costs, costs)]
   condition2 = [all(x < y for x, y in zip(cost[:-1], bids[:-1])) for cost in costs]
   if all(condition1) and all(condition2):
@@ -98,5 +101,6 @@ plt.figure()
 for c in costs:
   plt.plot(c, bids)
 plt.grid()
+plt.legend(['Bidder {}'.format(i) for i in range(n)], loc='upper left')
 plt.savefig('lebrun.pdf')
 
