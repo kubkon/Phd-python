c3281cf6535e925fc80b2e785014dd5b96598871
Jakub Konka <kubkon@gmail.com>
2013-02-21 18:28:23 +0000

    Add function for estimating upper bound on bids.

--- Auctions/Digital Marketplace/asymmetries/bajari_polynomial.hs
+++ Auctions/Digital Marketplace/asymmetries/bajari_polynomial.hs
@@ -26,6 +26,17 @@ upperExt :: Double -- price weight (w)
   -> [Double]      -- corresponding list of upper extremities
 upperExt w reps = map (\l -> l+w) $ lowerExt w reps
 
+-- Upper bound on bids
+upperBoundBidsFunc :: [Double] -- list of lower extremities
+  -> [Double]                  -- list of upper extremities
+  -> Double                    -- output estimate on upper bound on bids
+upperBoundBidsFunc lowers uppers =
+  let bs = NC.linspace 10000 ((head uppers), (uppers !! 1))
+      cdfs = zipWith (\l u -> uniformCDF l u) (drop 1 lowers) (drop 1 uppers)
+      negCdfs x = map (\cdf -> 1 - (cdf x)) cdfs
+      objective x = (x - (head uppers)) * (foldr1 (\c acc -> acc * c) $ negCdfs x)
+  in NC.atIndex bs $ NC.maxIndex $ DPV.mapVector objective bs
+
 -- (Scalar) cost function
 costFunc :: Double     -- lower bound on bids
   -> DPV.Vector Double -- vector of coefficients
@@ -48,17 +59,14 @@ derivCostFunc bLow cs b =
   in NC.dot cs bs
 
 -- FoC vector function
-focFunc :: Double       -- lower bound on bids
-  -> DPV.Vector Double  -- vector of coefficients
-  -> [Double -> Double] -- list of CDFs
-  -> [Double -> Double] -- list of PDFs
-  -> Double             -- bid value
-  -> DPV.Vector Double  -- output FoC vector (to be minimized)
-focFunc bLow cs cdfs pdfs b =
+focFunc :: Double        -- lower bound on bids
+  -> [DPV.Vector Double] -- list of vector of coefficients
+  -> [Double -> Double]  -- list of CDFs
+  -> [Double -> Double]  -- list of PDFs
+  -> Double              -- bid value
+  -> DPV.Vector Double   -- output FoC vector (to be minimized)
+focFunc bLow vCs cdfs pdfs b =
   let n = length cdfs
-      m = (DPV.dim cs) `div` (fromIntegral n)
-      indexes = [0,m..(m*(n-1))]
-      vCs = map (\i -> DPV.subVector i m cs) indexes
       costs = map (\x -> costFunc bLow x b) vCs
       derivCosts = DPV.fromList $ map (\x -> derivCostFunc bLow x b) vCs
       zips = zip cdfs pdfs
@@ -67,6 +75,27 @@ focFunc bLow cs cdfs pdfs b =
       consts = NC.constant ((sum $ DPV.toList rs) / ((fromIntegral n) - 1)) n
   in NC.sub derivCosts $ NC.mul probs $ NC.sub consts rs
 
+-- Lower boundary condition vector function
+lowerBoundFunc :: Double -- lower bound on bids
+  -> [DPV.Vector Double] -- list of vector of coefficients
+  -> [Double]            -- list of lower extremities
+  -> DPV.Vector Double   -- output lower boundary vector (to be minimized)
+lowerBoundFunc bLow vCs lowers =
+  let costs = DPV.fromList $ map (\x -> costFunc bLow x bLow) vCs
+      consts = DPV.fromList $ lowers
+  in NC.sub costs consts
+
+-- Upper boundary condition vector function
+upperBoundFunc :: Double -- lower bound on bids
+  -> Double              -- upper bound on bids
+  -> [DPV.Vector Double] -- list of vector of coefficients
+  -> DPV.Vector Double   -- output upper boundary vector (to be minimized)
+upperBoundFunc bLow bUpper vCs =
+  let n = length vCs
+      costs = DPV.fromList $ map (\x -> costFunc bLow x bUpper) vCs
+      consts = NC.constant bUpper n
+  in NC.sub costs consts
+
 -- Objective function
 objFunc :: Double   -- upper bound on bids
   -> [Double]       -- list of lower extremities
@@ -74,26 +103,37 @@ objFunc :: Double   -- upper bound on bids
   -> [Double]       -- parameters to estimate
   -> Double         -- value of the objective
 objFunc bUpper lowers uppers params =
-  let bLow = head params
+  let granularity = 100
+      bLow = head params
       cs = DPV.fromList $ drop 1 params
+      n = length cdfs
+      m = (DPV.dim cs) `div` (fromIntegral n)
+      indexes = [0,m..(m*(n-1))]
+      vCs = map (\i -> DPV.subVector i m cs) indexes
       cdfs = zipWith (\l u -> uniformCDF l u) lowers uppers
       pdfs = zipWith (\l u -> uniformPDF l u) lowers uppers
-      bs = NC.linspace 100 (bLow, bUpper)
-      focSq b = NC.sumElements $ DPV.mapVector (^^2) $ focFunc bLow cs cdfs pdfs b
+      bs = NC.linspace granularity (bLow, bUpper)
+      focSq b = NC.sumElements $ DPV.mapVector (^^2) $ focFunc bLow vCs cdfs pdfs b
       foc = NC.sumElements $ DPV.mapVector focSq bs
-  in foc
+      lowerBound = NC.sumElements $ DPV.mapVector (^^2) $ lowerBoundFunc bLow vCs lowers
+      upperBound = NC.sumElements $ DPV.mapVector (^^2) $ upperBoundFunc bLow bUpper vCs
+  in foc + (fromIntegral granularity) * lowerBound + (fromIntegral granularity) * upperBound
 
 -- Minimization
 main = do
-  let bUpper = 0.875
   let w = 0.75
   let reps = [0.25, 0.75]
-  let objective = objFunc bUpper (lowerExt w reps) (upperExt w reps)
+  let lowers = lowerExt w reps
+  let uppers = upperExt w reps
+  let bUpper = upperBoundBidsFunc lowers uppers
+  let objective = objFunc bUpper lowers uppers
   let l1 = (lowerExt w reps) !! 1
-  let (s,p) = GSL.minimize GSL.NMSimplex2 1E-8 1000 (take 13 [1E-2,1E-2..]) objective (take 13 ([l1] ++ [1E-2,1E-2..]))
+  let (s,p) = GSL.minimize GSL.NMSimplex2 1E-8 100000 (take 13 [1E-2,1E-2..]) objective (take 13 ([l1] ++ [1E-2,1E-2..]))
   let bLow = head s
+  putStr "Estimated lower bound on bids: "
+  print bLow
   let cs = DPV.fromList $ drop 1 s
-  let bs = DPV.toList $ NC.linspace 100 (bLow, bUpper)
+  let bs = DPV.toList $ NC.linspace 1000 (bLow, bUpper)
   CHART.plotPDF "polynomial.pdf" bs (costFunc bLow $ DPV.subVector 0 6 cs) "-" (costFunc bLow $ DPV.subVector 6 6 cs) "- "
 
 -- Tests
